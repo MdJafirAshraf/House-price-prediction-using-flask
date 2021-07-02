2339ab76273d32d644d99ce3efebb4dda0e3f7c4
Mohamed Jafir Ashraf <63150299+JafirDon@users.noreply.github.com>
2021-07-02 17:02:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    val1 = request.form['bedrooms']
+    val2 = request.form['bathrooms']
+    val3 = request.form['floors']
+    val4 = request.form['yr_built']
+    arr = np.array([val1, val2, val3, val4])
+    arr = arr.astype(np.float64)
+    pred = model.predict([arr])
+
+    return render_template('index.html', data=int(pred))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ house.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+
+df = pd.read_csv(
+    r'C:\Users\ELCOT\My Learning\deploy-ml-model-flask\House Price Prediction in Flask\house_data.csv')
+
+columns = ['bedrooms', 'bathrooms', 'floors', 'yr_built', 'price']
+df = df[columns]
+
+X = df.iloc[:, 0:4]
+y = df.iloc[:, 4:]
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+pickle.dump(lr, open('model.pkl', 'wb'))
